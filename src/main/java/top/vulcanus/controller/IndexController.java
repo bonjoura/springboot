1c9d3dd131118268033b12891aa17a82bc2d727d
springboot <bonjoura@163.com>
2020-04-14 21:39:55 +0800

    index added

---
+++ src/main/java/top/vulcanus/controller/IndexController.java
@@ -0,0 +1,16 @@
+package top.vulcanus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String inex() {return "index";}
+//    public  String hello(@RequestParam(name = "name") String name , Model model){
+//        model.addAttribute("name",name);//kv pair
+//        return "hello";
+//    }
+}
