2141a7a2dad2910e455ce6a4915bd7489920a76e
springboot <bonjoura@163.com>
2020-04-23 19:20:08 +0800

    extract github config from properties

---
+++ src/main/java/top/vulcanus/controller/AuthController.java
@@ -0,0 +1,40 @@
+package top.vulcanus.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import top.vulcanus.dto.AccessTokenDTO;
+import top.vulcanus.dto.GithubUser;
+import top.vulcanus.provider.GithubProvider;
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String clientUri;
+
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setClient_id("4675dfc27ee7cde61639");
+        accessTokenDTO.setClient_secret("203f21a6540e1b6e6fd922b31d9128d3dc4d99f1");
+        accessTokenDTO.setState(state);
+
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
